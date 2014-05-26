3fa6f3de3476d5baa9858ed87edd50b42b7f5ab9
Rwll <rowell.dcanlapan@gmail.com>
2014-05-26 18:02:54 +0800

    joomla commit

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-05-25 06:40:44 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-05-25T06:40:44+00:00	INFO	cookiefailure	Username and password do not match or you do not have an account yet.
+2014-05-25T06:40:47+00:00	INFO	cookiefailure	Username and password do not match or you do not have an account yet.
